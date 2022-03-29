752e8fcbce04735e4de966f24840e603da8df04a
jodyphelan <jody.phelan@lshtm.ac.uk>
2022-03-29 18:05:43 +0100

    kmc threading

--- malaria_profiler/speciation.py
+++ malaria_profiler/speciation.py
@@ -13,14 +13,14 @@ def speciate(args):
     else:
         if args.mitochondria_seq_name:
             pp.run_cmd(f"samtools view -b {args.bam} {args.mitochondria_seq_name} | samtools fastq > {args.files_prefix}.tmp.fq")
-            kmer_dump = pp.fastq(f"{args.files_prefix}.tmp.fq").get_kmer_counts(args.files_prefix)
+            kmer_dump = pp.fastq(f"{args.files_prefix}.tmp.fq").get_kmer_counts(args.files_prefix,threads=args.threads)
         else:
             bam = pp.bam(args.bam,args.files_prefix,"illumina")
             if bam.filetype=="cram":
                 pp.run_cmd(f"samtools fastq {bam.bam_file} > {args.files_prefix}.tmp.fq")
-                kmer_dump = pp.fastq(f"{args.files_prefix}.tmp.fq").get_kmer_counts(args.files_prefix)
+                kmer_dump = pp.fastq(f"{args.files_prefix}.tmp.fq").get_kmer_counts(args.files_prefix,threads=args.threads)
             else:
-                kmer_dump = bam.get_kmer_counts(args.files_prefix)
+                kmer_dump = bam.get_kmer_counts(args.files_prefix,threads=args.threads)
     
     species = kmer_dump.get_taxonomic_support(conf['kmers'])
     return species
