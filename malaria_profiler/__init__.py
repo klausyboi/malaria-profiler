f27a85b7d599b849c5336c56c6ce5aefe446b393
jodyphelan <jody.phelan@lshtm.ac.uk>
2022-03-29 15:41:46 +0100

    update speciation

--- malaria_profiler/__init__.py
+++ malaria_profiler/__init__.py
@@ -2,4 +2,5 @@ from .utils import *
 from .reformat import *
 from .output import *
 from .profiler import *
+from .speciation import *
 __version__="0.0.1"
---
+++ malaria_profiler/speciation.py
@@ -0,0 +1,26 @@
+import pathogenprofiler as pp
+
+def speciate(args):
+    if args.external_species_db:
+        conf = pp.get_species_db(args.software_name,args.external_species_db)
+    else:
+        conf = pp.get_species_db(args.software_name,args.species_db)
+    
+    if args.bam==None:
+        fastq = pp.fastq(args.read1,args.read2)
+        kmer_dump = fastq.get_kmer_counts(args.files_prefix)
+
+    else:
+        if args.mitochondria_seq_name:
+            pp.run_cmd(f"samtools view -b {args.bam} {args.mitochondria_seq_name} | samtools fastq > {args.files_prefix}.tmp.fq")
+            kmer_dump = pp.fastq(f"{args.files_prefix}.tmp.fq").get_kmer_counts(args.files_prefix)
+        else:
+            bam = pp.bam(args.bam,args.files_prefix,"illumina")
+            if bam.filetype=="cram":
+                pp.run_cmd(f"samtools fastq {bam.bam_file} > {args.files_prefix}.tmp.fq")
+                kmer_dump = pp.fastq(f"{args.files_prefix}.tmp.fq").get_kmer_counts(args.files_prefix)
+            else:
+                kmer_dump = bam.get_kmer_counts(args.files_prefix)
+    
+    species = kmer_dump.get_taxonomic_support(conf['kmers'])
+    return species
